cc25bafc521ea0b2efc69bacf4d95f905ad50d08
AlbertUnruh <AlbertUnruh@pm.me>
2023-03-18 14:45:40 +0100

    update config and add __init__.py

---
+++ AlbertoX3/__init__.py
@@ -0,0 +1,5 @@
+__author__ = "AlbertUnruh"
+__version__ = "0.0.0a"
+__license__ = "GNU AGPLv3"
+__copyright__ = f"Copyright 2023-present (c) {__author__} - Alberto-X3"
+__url__ = "https://github.com/Alberto-X3/Alberto-X3-V4"
